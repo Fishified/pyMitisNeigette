adbaecf55b4f2cf29ab9ce119ade06e3a4fdc9b6
Jason M Duguay <duguay.jason@gmail.com>
2020-11-23 14:46:51 -0500

    Update exampleUsageScript.py

--- exampleUsageScript.py
+++ exampleUsageScript.py
@@ -1,7 +1,4 @@
 import pyMitisNeigette.run as run
-import numpy as np
-import pandas as pd
-import math
 
 proc=run.Run(r'Z:\Jason\Projects\2020\MitisNeigette\CFD\MainSims\main_split2Smag',vtp=True,dCorrs=True,dd='h5')
 
